eaaeecfd6c1f7c1fe05e8211a18d73b8650dd349
noordic <nicolasoporto@gmail.com>
2022-07-28 23:34:59 -0400

    minor fix

--- server/src/models/contacto.js
+++ server/src/models/contacto.js
@@ -8,6 +8,7 @@ const contactoSchema = new Schema(
     segApellido: { type: String, required: true },
     email: { type: String, required: true },
     telefono: { type: String, required: true },
+    flag: {type: Boolean, required: false},
   },
   {
     versionKey: false,
